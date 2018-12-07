85f539e5f5c984ae591c4f44415ce619b7c3e28a
ime <ime@MAT-782183>
2018-12-06 23:11:59 -0300

    Teste

--- src/bolao/execucao/Main.java
+++ src/bolao/execucao/Main.java
@@ -19,17 +19,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Pessoa administrador = new Administrador().createAccount("Admin", "admin", "123");
-        Pessoa apostador1 = new Apostador().createAccount("Apostador1", "apostador1", "123");
-        Pessoa apostador2 = new Apostador().createAccount("Apostador2", "apostador2", "123");
+        Pessoa administrador = new Administrador().createAccount("Teste", "admin", "123");
 
         Jogo jogo = new Jogo();
         ControlBolao bolao = new ControlBolao(jogo);
 
         bolao.registerObserver(administrador);
-        bolao.registerObserver(apostador1);
-        bolao.registerObserver(apostador2);
-
         jogo.setCommand(bolao);
         jogo.setCommand(bolao);
 
